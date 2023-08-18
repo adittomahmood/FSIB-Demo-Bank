32ce2a2e38b864627acc0523ac3bb8b8ec8b9ad2
adittomahmood <adittomahmood@gmail.com>
2023-08-18 23:29:24 +0600

    functioned deposit button

--- deposit.js
+++ deposit.js
@@ -3,7 +3,12 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   let previousAmount = parseFloat(
     document.getElementById("total-deposit").innerText
   );
-  let currenntAmount = previousAmount + depositAmount;
-  document.getElementById("total-deposit").innerText = currenntAmount;
+  let currentAmount = previousAmount + depositAmount;
+  document.getElementById("total-deposit").innerText = currentAmount.toFixed(2);
   document.getElementById("deposit").value = "";
+  let currentBalance =
+    parseFloat(document.getElementById("total-balance").innerText) +
+    depositAmount;
+  document.getElementById("total-balance").innerText =
+    currentBalance.toFixed(2);
 });
